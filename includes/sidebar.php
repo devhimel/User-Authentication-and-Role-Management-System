3dff72f2fa798e5f63068f49cae8f22fbcd22296
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-10-24 13:03:57 +0600

    logo added

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,8 +3,8 @@
     aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <a href="https://flowbite.com/" class="flex items-center pl-2.5 mb-5">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-7" alt="Flowbite Logo" />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <img src="../logo.png" class="h-6 mr-3 sm:h-7" alt="Logo" />
+
         </a>
         <ul class="space-y-2 font-medium">
             <li>
