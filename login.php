b59dfbab7ef11baca5aa5b7252bc1926d39871d4
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-10-28 13:21:19 +0600

    loging error message issus fixed

--- login.php
+++ login.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
-$email = $_POST['email'];
-$password = $_POST['password'];
-$errorMessage = '';
+
 $fp = fopen("./database/user_list.txt", "r");
 $roles = array();
 $emails = array();
@@ -18,6 +16,11 @@ while (!feof($fp)) {
     $names[] = trim($userData[3]);
 }
 fclose($fp);
+
+if(isset($_POST['submit'])){
+    $email = $_POST['email'];
+$password = $_POST['password'];
+$errorMessage = '';
 for ($i = 0; $i < count($roles); $i++) {
     if($roles[$i] == 'admin'){
         if($emails[$i] == $email && $passwords[$i] == $password){
@@ -46,6 +49,9 @@ for ($i = 0; $i < count($roles); $i++) {
     }
     
 }
+}
+
+
 
 ?>
 <!DOCTYPE html>
@@ -79,9 +85,10 @@ for ($i = 0; $i < count($roles); $i++) {
                         required>
                 </div>
                 <p class="text-red-600">
-                    <?php echo $errorMessage; ?>
+                    <?php if(strlen($errorMessage)>0){
+                        echo $errorMessage;} ?>
                 </p>
-                <button type="submit"
+                <button type="submit" name="submit"
                     class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login
                     to your account</button>
                 <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
